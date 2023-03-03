ea947dff7674c54e6636038bb950141b5b4e7cd8
ihsan_elif <ihsan_elif@AN515-52>
2023-03-03 17:54:03 +0100

    homeworkThree commiti

---
+++ src/test/java/pages/HomeworkThreePages.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HomeworkThreePages {
+    public HomeworkThreePages(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy (id="onetrust-accept-btn-handler")
+    public WebElement cookiesAccept;
+    @FindBy (xpath = "(//a[@class='TK-Menu-Item-Link'])[5]")
+    public WebElement pricingLink;
+    @FindBy (xpath = "(//a[@class='Btn Btn--prim4 u-db'])[1]")
+    public WebElement buyNowButtonFirst;
+    @FindBy (xpath = "(//a[@class='Btn Btn--prim4 u-db'])[2]")
+    public WebElement buyNowButtonSecond;
+    @FindBy(xpath = "//a[@class='referrer-link']")
+    public WebElement continueShoppingLink;
+    @FindBy (xpath = "//a[@class='TK-TLRK-Logo']")
+    public WebElement goBackHomePage;
+    @FindBy (xpath = "//a[@title='Shopping cart']")
+    public WebElement basketItem;
+    @FindBy (xpath = "(//a[@class='e2e-delete-item btn-delete-item ng-star-inserted'])[1]")
+    public WebElement deleteLinkFirst;
+
+}
---
+++ src/test/java/stepdefinitions/HomeworkThreeSteps.java
@@ -0,0 +1,89 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+import pages.HomeworkThreePages;
+import utilities.CommenSteps;
+import utilities.Driver;
+
+public class HomeworkThreeSteps {
+    HomeworkThreePages homeworkThreePages = new HomeworkThreePages();
+     int count = 0;
+    String actualCount;
+    String expectedCount;
+
+    @Given("kullanici {string} web sayfasina gider")
+    public void kullaniciWebSayfasinaGider(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    @When("kullanici cookiesi kabul eder")
+    public void kullaniciCookiesiKabulEder() {
+        try{
+            CommenSteps.waitForVisibility(homeworkThreePages.cookiesAccept,5);
+            homeworkThreePages.cookiesAccept.click();
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
+
+    @And("kullanici Pricing linkine tiklar")
+    public void kullaniciPricingLinkineTiklar() {
+        homeworkThreePages.pricingLink.click();
+    }
+
+    @And("kullanici BuyNow butonu ile sepete bir urun ekler")
+    public void kullaniciBuyNowButonuIleSepeteBirUrunEkler() {
+        homeworkThreePages.buyNowButtonSecond.click();
+        count +=1;
+
+    }
+
+    @And("kullanici Pricing sayfasina geri doner")
+    public void kullaniciPricingSayfasinaGeriDoner() {
+        CommenSteps.waitForVisibility(homeworkThreePages.continueShoppingLink,5);
+        homeworkThreePages.continueShoppingLink.click();
+    }
+
+    @And("kullanici Buynow butonu ile bir urun daha ekler")
+    public void kullaniciBuynowButonuIleBirUrunDahaEkler() {
+        homeworkThreePages.buyNowButtonFirst.click();
+        count+=1;
+    }
+
+    @And("kullanici ana sayfaya geri doner")
+    public void kullaniciAnaSayfayaGeriDoner() {
+        CommenSteps.waitForVisibility(homeworkThreePages.continueShoppingLink,5);
+        homeworkThreePages.goBackHomePage.click();
+    }
+
+    @And("kullanici sepete uzerindeki mevcut sayiyi alir")
+    public void kullaniciSepeteUzerindekiMevcutSayiyiAlir() {
+        actualCount = homeworkThreePages.basketItem.getText();
+        expectedCount = String.valueOf(count);
+        Assert.assertEquals(actualCount,expectedCount);
+    }
+
+    @And("kullanici sepete tekrar gider")
+    public void kullaniciSepeteTekrarGider() {
+        homeworkThreePages.basketItem.click();
+    }
+
+    @And("kullanici sepetteki urunlerden bir tanesini Delete linki ile siler")
+    public void kullaniciSepettekiUrunlerdenBirTanesiniDeleteLinkiIleSiler() {
+        CommenSteps.waitForVisibility(homeworkThreePages.continueShoppingLink,5);
+        homeworkThreePages.deleteLinkFirst.click();
+        count-=1;
+    }
+
+    @Then("kullanici sepet simgesi uzerindeki sayinin bir eksildigini kontrol eder")
+    public void kullaniciSepetSimgesiUzerindekiSayininBirEksildiginiKontrolEder() {
+        actualCount = homeworkThreePages.basketItem.getText();
+        expectedCount = String.valueOf(count);
+        Assert.assertEquals(actualCount,expectedCount);
+    }
+}
